14c1ca52d59ddd7e5962f7cdec5db330948ad9c5
Andrew Munsell <andrew@wizardapps.net>
2015-05-09 11:44:21 -0700

    Add errors

---
+++ src/errors/InvalidUsernamePasswordError.js
@@ -0,0 +1,10 @@
+/**
+ * @package admiral
+ * @author Andrew Munsell <andrew@wizardapps.net>
+ * @copyright 2015 WizardApps
+ */
+
+var SubclassError = require('subclass-error'),
+    Promise = require('bluebird');
+
+module.exports = SubclassError('InvalidUsernamePasswordError', Promise.OperationalError);
