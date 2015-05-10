fe17fcce9e6cdbfe7910e4a4198366b0b7b8e2af
Andrew Munsell <andrew@wizardapps.net>
2015-05-09 21:52:04 -0700

    Add CORS headers

--- src/app.js
+++ src/app.js
@@ -10,6 +10,13 @@ var bodyParser = require('body-parser');
 exports = module.exports = function() {
     var app = express();
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        next();
+    });
+
     app.use(express.static(__dirname + '/../public'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
