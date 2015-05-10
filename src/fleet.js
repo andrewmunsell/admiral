86e7f627c32c81ce1fbe455acf025e61707f9f7b
Andrew Munsell <andrew@wizardapps.net>
2015-05-09 21:22:12 -0700

    Add Fleet helper

---
+++ src/fleet.js
@@ -0,0 +1,24 @@
+/**
+ * @package admiral
+ * @author Andrew Munsell <andrew@wizardapps.net>
+ * @copyright 2015 WizardApps
+ */
+
+var Fleetctl = require('fleetctl'),
+    Promise = require('bluebird');
+
+exports = module.exports = function(nconf) {
+    var options = {};
+
+    if(nconf.get('t')) {
+        options.tunnel = nconf.get('t');
+        options['ssh-username'] = nconf.get('tunnel-username');
+    }
+
+    var fleetctl = new Fleetctl(options);
+
+    return Promise.promisifyAll(fleetctl);
+};
+
+exports['@singleton'] = true;
+exports['@require'] = ['nconf'];
