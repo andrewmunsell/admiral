129c1f373aa004e6cd95fe9114714fbd6ee3e861
Andrew Munsell <andrew@wizardapps.net>
2015-05-18 12:53:38 -0700

    Use new services model helper

--- src/routes/services.js
+++ src/routes/services.js
@@ -4,31 +4,24 @@
  * @copyright 2015 WizardApps
  */
 
-exports = module.exports = function(app, config) {
+exports = module.exports = function(app, config, Services) {
     /**
      * Get the services
      */
     app.get('/v1/services', function(req, res) {
-        config.getAsync('/services', { recursive: true })
-            .spread(function(result) {
+        Services.all()
+            .then(function(services) {
                 res
                     .header('Cache-Control', 'private, max-age=60')
-                    .json((result.node.nodes || [] ).map(function(node) {
-                        return JSON.parse(node.value);
-                    }));
+                    .json(services);
             })
             .catch(function(err) {
-                if(err.errorCode == 100) {
-                    res
-                        .json([]);
-                } else {
-                    res
-                        .status(500)
-                        .json({
-                            error: 500,
-                            message: 'There was a problem retrieving the list of services.'
-                        });
-                }
+                res
+                    .status(500)
+                    .json({
+                        error: 500,
+                        message: 'There was a problem retrieving the list of services.'
+                    });
             })
             .finally(function() {
                 res.end();
@@ -37,4 +30,4 @@ exports = module.exports = function(app, config) {
 };
 
 exports['@singleton'] = true;
-exports['@require'] = ['app', 'config'];
+exports['@require'] = ['app', 'config', 'models/Services'];
