035f5bd757259143f1689579205eee2a49e40b49
Andrew Munsell <andrew@wizardapps.net>
2015-05-19 20:42:08 -0700

    Add endpoint to change a service's state

--- src/routes/services.js
+++ src/routes/services.js
@@ -31,6 +31,64 @@ exports = module.exports = function(app, config, Log, Services) {
                 res.end();
             });
     });
+
+    /**
+     * Change a service's state
+     */
+    app.post('/v1/services/:id/state', function(req, res) {
+        Services.get(req.params.id)
+            .then(function(service) {
+                if (service == null) {
+                    return res
+                        .status(404)
+                        .json({
+                            error: 404,
+                            message: 'The specified service does not exist.'
+                        });
+                }
+
+                return service;
+            })
+            .then(function(service) {
+                var ServiceTemplate = IoC.create('service-templates/' + toCase.pascal(service.template));
+
+                switch(req.params.state) {
+                    case 'start':
+                        return ServiceTemplate.start(service);
+                    case 'stop':
+                        return ServiceTemplate.stop(service);
+                    case 'terminate':
+                        return ServiceTemplate.terminate(service);
+                    default:
+                        return res
+                            .status(400)
+                            .json({
+                                error: 400,
+                                message: 'An invalid state was specified.'
+                            });
+                }
+            })
+            .then(function(service) {
+                return res
+                    .header('Cache-Control', 'no-cache')
+                    .json(service);
+            })
+            .catch(function(err) {
+                Log.error('There was a problem changing the service\'s state.', {
+                    context: err
+                });
+
+                res
+                    .status(500)
+                    .json({
+                        error: 500,
+                        message: 'There was a problem retrieving the list of services.'
+                    });
+            })
+            .finally(function() {
+                res.end();
+            });
+    });
 };
 
 exports['@singleton'] = true;
