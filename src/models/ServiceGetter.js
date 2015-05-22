21057b69297a8dd05408a4965b3d9ea8413a3981
Andrew Munsell <andrew@wizardapps.net>
2015-05-22 16:27:29 -0700

    Get the actual units for running unit files

--- src/models/ServiceGetter.js
+++ src/models/ServiceGetter.js
@@ -6,7 +6,7 @@
 
 var Promise = require('bluebird');
 
-exports = module.exports = function(config) {
+exports = module.exports = function(config, fleetctl) {
     var ServiceGetter = {
         /**
          * Get a single service, or null if the service does not exist.
@@ -16,6 +16,29 @@ exports = module.exports = function(config) {
             return config.getAsync('/services/' + id)
                 .spread(function(result) {
                     return JSON.parse(result.node.value);
+                })
+                .then(function(service) {
+                    return Promise.all([service, fleetctl.list_unitsAsync()]);
+                })
+                .spread(function(service, unitFiles) {
+                    var unitFileIds = service.unitFiles.map(function(unitFile) {
+                        return unitFile.id;
+                    });
+
+                    unitFiles.forEach(function(unitFile) {
+                        var id = unitFile.unit.split('@')[0];
+                        var i = unitFileIds.indexOf(id);
+
+                        if(i > -1) {
+                            if(typeof(service.unitFiles[i].units) == 'undefined') {
+                                service.unitFiles[i].units = [];
+                            }
+
+                            service.unitFiles[i].units.push(unitFile);
+                        }
+                    });
+
+                    return service;
                 })
                 .catch(function(err) {
                     if(err.errorCode == 100) {
@@ -31,4 +54,4 @@ exports = module.exports = function(config) {
 };
 
 exports['@singleton'] = true;
-exports['@require'] = ['config'];
+exports['@require'] = ['config', 'fleet'];
